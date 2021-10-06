1e27c0dc34429ed24a61adc72dc855b5f6348cc0
Matthew Dean Furlo <MDF@matthews-mbp.attlocal.net>
2021-10-06 16:42:02 -0700

    moving stuff into main.js

--- assets/main.js
+++ assets/main.js
@@ -57,6 +57,7 @@ function showInfo( [ accounts, venue, floor, latitude, longitude ] ) {
 
     console.log('Atrius data: ', accounts, venue, floor, latitude, longitude)
 
+    
     let firstAccount;
     try {
         firstAccount = JSON.parse(accounts)[0];
@@ -68,6 +69,30 @@ function showInfo( [ accounts, venue, floor, latitude, longitude ] ) {
             firstAccount = accounts.substring(0, accounts.indexOf(',')).trim();
         }
     }
+
+    var config = {
+        venueId: venue,
+        accountId: firstAccount
+    }
+
+
+    LMInit.setLogging(true);
+    LMInit.newMap('.locusmaps', config)
+        .then(map => {
+            map.setPosition({ lat: latitude, lng: longitude, floorId: floor, zoom: 17})
+            return map.getPosition()
+                .then(pos => map.drawMarker(
+                    "Map Note Placement",
+                    {
+                        lat: latitude,
+                        lng: longitude,
+                        ord: pos.ord
+                    },
+                    'https://img.locuslabs.com/js/misc/map-note-pin.svg'
+                ))
+        })
+        
+
 
     console.log('Atrius firstAccount: ', firstAccount)
 
@@ -84,23 +109,23 @@ function showInfo( [ accounts, venue, floor, latitude, longitude ] ) {
     var html = template(requester_data);
     $("#content").html(html);
 
-    setTimeout(
-        () => {
-            //window.LocusMaps({command: 'version'}).then(console.log)
-            //window.LocusMaps({command: "drawMarker", name: "Map Note Placement", lat: latitude, lng: longitude, imageURL: `https://img.locuslabs.com/js/misc/map-note-pin.svg`})
-            window.LLMap.getPosition()
-                .then(pos => window.LLMap.drawMarker(
-                    "Map Note Placement",
-                    {
-                        lat: latitude,
-                        lng: longitude,
-                        ord: pos.ord
-                    },
-                    'https://img.locuslabs.com/js/misc/map-note-pin.svg'
-                    ))
-        },
-        5000
-    )
+    // setTimeout(
+    //     () => {
+    //         //window.LocusMaps({command: 'version'}).then(console.log)
+    //         //window.LocusMaps({command: "drawMarker", name: "Map Note Placement", lat: latitude, lng: longitude, imageURL: `https://img.locuslabs.com/js/misc/map-note-pin.svg`})
+    //         LLMap.getPosition()
+    //             .then(pos => LLMap.drawMarker(
+    //                 "Map Note Placement",
+    //                 {
+    //                     lat: latitude,
+    //                     lng: longitude,
+    //                     ord: pos.ord
+    //                 },
+    //                 'https://img.locuslabs.com/js/misc/map-note-pin.svg'
+    //                 ))
+    //     },
+    //     5000
+    // )
 }
 
 function showError(response) {
